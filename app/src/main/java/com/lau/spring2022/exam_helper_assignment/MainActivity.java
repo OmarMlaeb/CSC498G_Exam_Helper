df9c605836bdf384daa832ff2c86c86dd1a58c7d
Omar Malaeb <omarmlb002@gmail.com>
2022-05-04 14:47:10 +0300

    added a list view

--- app/src/main/java/com/lau/spring2022/exam_helper_assignment/MainActivity.java
+++ app/src/main/java/com/lau/spring2022/exam_helper_assignment/MainActivity.java
@@ -3,12 +3,37 @@ package com.lau.spring2022.exam_helper_assignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView list;
+    ArrayList<String> array_list;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list = (ListView) findViewById(R.id.myList);
+
+        array_list = new ArrayList<String>(Arrays.asList());
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, array_list);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
     }
 }
