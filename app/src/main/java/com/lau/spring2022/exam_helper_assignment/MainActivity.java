393c29b35aa58df225d3d951fd6935859f8ecf8a
Omar Malaeb <omarmlb002@gmail.com>
2022-05-04 23:20:51 +0300

    connected each item in the list to the webview

--- app/src/main/java/com/lau/spring2022/exam_helper_assignment/MainActivity.java
+++ app/src/main/java/com/lau/spring2022/exam_helper_assignment/MainActivity.java
@@ -2,6 +2,7 @@ package com.lau.spring2022.exam_helper_assignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -33,7 +34,19 @@ public class MainActivity extends AppCompatActivity {
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(MainActivity.this, WebActivity.class);
 
+                if(array_list.get(i).equals("Mobile Computing")){
+                    intent.putExtra("url", "https://ionicframework.com/");
+                } else if(array_list.get(i).equals("Software Engineering")){
+                    intent.putExtra("url", "https://geekflare.com/software-engineering-courses/");
+                } else if(array_list.get(i).equals("Discrete II")){
+                    intent.putExtra("url", "https://web.stanford.edu/class/cs103x/cs103x-notes.pdf");
+                } else if(array_list.get(i).equals("Database Management Systems")){
+                    intent.putExtra("url", "https://onlinecourses.nptel.ac.in/noc22_cs51/preview");
+                }
+
+                startActivity(intent);
             }
         });
 
@@ -42,9 +55,12 @@ public class MainActivity extends AppCompatActivity {
             SQLiteDatabase sql = this.openOrCreateDatabase("laudb", MODE_PRIVATE, null);
             sql.execSQL("CREATE Table IF NOT EXISTS exams (exam_name VARCHAR primary key)");
 
+            //sql.execSQL("INSERT INTO exams(exam_name) VALUES ('Mobile Computing')");
             //sql.execSQL("INSERT INTO exams(exam_name) VALUES ('Software Engineering')");
+            //sql.execSQL("INSERT INTO exams(exam_name) VALUES ('Discrete II')");
+            //sql.execSQL("INSERT INTO exams(exam_name) VALUES ('Database Management Systems')");
 
-            //sql.execSQL("DELETE FROM exams where exam_name  = 'Mobile Computing'");
+            //sql.execSQL("DELETE FROM exams where exam_name = 'Biology'");
 
             Cursor c = sql.rawQuery("Select * from exams", null);
             int exam_nameIndex = c.getColumnIndex("exam_name");
--- app/src/main/java/com/lau/spring2022/exam_helper_assignment/WebActivity.java
+++ app/src/main/java/com/lau/spring2022/exam_helper_assignment/WebActivity.java
@@ -2,7 +2,10 @@ package com.lau.spring2022.exam_helper_assignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -11,11 +14,20 @@ public class WebActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_web);
+
+        Intent intent = getIntent();
+
+        String url = intent.getStringExtra("url");
 
         WebView view = (WebView) findViewById(R.id.webview);
         view.getSettings().setJavaScriptEnabled(true);
         view.setWebViewClient( new WebViewClient());
-        view.loadUrl("");
+        view.loadUrl(url);
     }
 }
